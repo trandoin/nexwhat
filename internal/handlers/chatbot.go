87c05025abcfe2106c689c0ced2d050a0077792f
trandoin <querytrando2020@gmail.com>
2026-09-14 12:39:44 +0530

    fix(build): add missing database package import in chatbot handler

--- internal/handlers/chatbot.go
+++ internal/handlers/chatbot.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/shridarpatil/whatomate/internal/audit"
+	"github.com/shridarpatil/whatomate/internal/database"
 	"github.com/shridarpatil/whatomate/internal/models"
 	"github.com/valyala/fasthttp"
 	"github.com/zerodha/fastglue"
